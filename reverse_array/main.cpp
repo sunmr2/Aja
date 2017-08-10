0c36dcedf21dccbc7a6dcc952e817c5f70483e51
Sun Yongding <ysun5@mail.csuchico.edu>
2017-08-10 07:57:31 -0700

    hi

---
+++ reverse_array/main.cpp
@@ -0,0 +1,13 @@
+
+#include <map>
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+  string w = "hello";
+  reverse(w.begin(),w.end());
+  cout<< w <<endl;
+  return 0;
+}
